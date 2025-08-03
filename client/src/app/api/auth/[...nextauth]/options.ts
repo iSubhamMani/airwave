6b59a7e1cf6af9c1112ffd288c3ca638bbbc73fd
Subham Mani <manisubham09@gmail.com>
2025-08-04 00:20:48 +0530

    add authentication options and type definitions for NextAuth

---
+++ client/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { db } from "@/db";
+import { usersTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        try {
+          const { email, password } = credentials;
+
+          if (!email || !password) {
+            throw new Error("Email and password are required");
+          }
+
+          // Find user in the database
+          const users = await db
+            .select()
+            .from(usersTable)
+            .where(eq(usersTable.email, email));
+
+          if (users.length === 0) {
+            throw new Error("No user found with this email");
+          }
+
+          const user = users[0];
+
+          if (!user.isVerified) {
+            throw new Error("email_not_verified");
+          }
+
+          // check password
+          const isPasswordValid = await bcrypt.compare(password, user.password);
+          if (!isPasswordValid) {
+            throw new Error("Invalid password");
+          }
+
+          return user;
+        } catch (error: any) {
+          throw new Error(error.message);
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id?.toString();
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
+};
