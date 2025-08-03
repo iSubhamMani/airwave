5fb58c9c3b3b2d9d9c50a0d225dfffc02dbcf3fa
Subham Mani <manisubham09@gmail.com>
2025-08-04 00:19:56 +0530

    add ui components

---
+++ client/src/components/Toast.ts
@@ -0,0 +1,19 @@
+export const successStyle = {
+  backgroundColor: "#b9f8cf",
+  backdropFilter: "blur(8px)",
+  color: "oklch(52.7% 0.154 150.069)",
+  padding: "10px 20px",
+  borderRadius: "10px",
+  fontSize: "14px",
+  fontWeight: "bold",
+};
+
+export const errorStyle = {
+  backgroundColor: "#ffb4b4",
+  backdropFilter: "blur(8px)",
+  color: "rgba(210, 0, 0, 1)",
+  padding: "10px 20px",
+  borderRadius: "10px",
+  fontSize: "14px",
+  fontWeight: "bold",
+};
