0010465f62434764dddafac842706499374d582a
Subham Mani <manisubham09@gmail.com>
2025-08-04 00:21:39 +0530

    add email verification component

---
+++ client/src/app/(auth)/verify/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { InteractiveHoverButton } from "@/components/InteractiveHoverButton";
+import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { Label } from "@/components/ui/label";
+import { useAuth } from "@/store/auth";
+import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
+import { useState } from "react";
+
+const VerifyEmail = () => {
+  const [otp, setOtp] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const { emailSent } = useAuth((store) => store);
+
+  return (
+    <>
+      <CardHeader>
+        <CardTitle className="text-lg sm:text-xl font-bold text-white">
+          Verify Your Email
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-6">
+        {emailSent ? (
+          <div className="space-y-6">
+            <InputOTP
+              value={otp}
+              onChange={(value) => setOtp(value)}
+              maxLength={6}
+              pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
+            >
+              <InputOTPGroup className="flex gap-2 justify-center">
+                {[...Array(6)].map((_, index) => (
+                  <InputOTPSlot
+                    key={index}
+                    index={index}
+                    className="w-12 h-14 text-center text-white text-xl bg-white/15 
+              border-x-0 border-t-0 border-b border-white rounded-sm 
+              focus:outline-none focus:ring-2 focus:ring-white"
+                  />
+                ))}
+              </InputOTPGroup>
+            </InputOTP>
+            <p className="text-sm text-center text-white">
+              Didn&apos;t receive the OTP?{" "}
+              <span className="text-green-200 font-bold cursor-pointer hover:underline">
+                Resend OTP
+              </span>
+            </p>
+          </div>
+        ) : (
+          <div className="space-y-2">
+            <Label
+              htmlFor="email"
+              className="text-green-200 text-xs sm:text-sm"
+            >
+              Registered Email
+            </Label>
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              type="email"
+              placeholder="Enter your registered email"
+              className="text-xs sm:text-sm text-white"
+            />
+          </div>
+        )}
+
+        <InteractiveHoverButton className="text-xs sm:text-sm">
+          {emailSent ? "Verify OTP" : "Send OTP"}
+        </InteractiveHoverButton>
+      </CardContent>
+    </>
+  );
+};
+
+export default VerifyEmail;
