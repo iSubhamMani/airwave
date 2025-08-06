f0993398245b57951e0615e4785f2ca728151575
Subham Mani <manisubham09@gmail.com>
2025-08-06 21:02:41 +0530

    add middleware

---
+++ client/src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export { default } from "next-auth/middleware";
+
+const protectedRoutes = ["/dashboard"];
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXT_AUTH_SECRET! });
+
+  const currentUrl = req.nextUrl;
+
+  if (!token && protectedRoutes.includes(currentUrl.pathname)) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (
+    (token && currentUrl.pathname === "/login") ||
+    currentUrl.pathname === "/signup" ||
+    currentUrl.pathname === "/verify"
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/login", "/signup", "/verify"],
+};
