4777f356c929d589893b304abf2daecd2da1c4e5
Subham Mani <manisubham09@gmail.com>
2025-08-25 19:45:18 +0530

    add peer service

---
+++ client/src/services/peer.ts
@@ -0,0 +1,46 @@
+class PeerService {
+  public peer: RTCPeerConnection | null = null;
+
+  constructor() {
+    if (!this.peer) {
+      this.peer = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: [
+              "stun:stun.l.google.com:19302",
+              "stun:global.stun.twilio.com:3478",
+            ],
+          },
+        ],
+      });
+    }
+  }
+
+  async getOffer() {
+    if (this.peer) {
+      const offer = await this.peer.createOffer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+
+      return offer;
+    }
+  }
+
+  async getAnswer(offer: RTCSessionDescriptionInit) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(new RTCSessionDescription(offer));
+      const ans = await this.peer.createAnswer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(ans));
+
+      return ans;
+    }
+  }
+
+  async setAnswer(ans: RTCSessionDescriptionInit) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
+    }
+  }
+}
+
+const peerService = new PeerService();
+export default peerService;
