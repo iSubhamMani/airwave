a11e81432bc81fbb44fb43a74146eee9bb10fd17
Subham Mani <manisubham09@gmail.com>
2025-08-25 19:45:05 +0530

    add ui components and update styling

--- client/src/app/(main)/dashboard/page.tsx
+++ client/src/app/(main)/dashboard/page.tsx
@@ -64,7 +64,7 @@ const Dashboard = () => {
                 />
                 <Button
                   disabled={!meetingId}
-                  className="w-full text-xs sm:text-sm group bg-white/85 text-green-700 hover:bg-white/90 font-semibold cursor-pointer shadow-xl transition-all duration-150 ease-in-out"
+                  className="w-full text-xs sm:text-sm group bg-white text-green-700 hover:bg-white/90 font-semibold cursor-pointer shadow-xl transition-all duration-150 ease-in-out"
                 >
                   <Users className="w-5 h-5 mr-2 group-hover:scale-110 transition-transform" />
                   Join Podcast
