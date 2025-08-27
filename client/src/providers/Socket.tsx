6e85f241d94bad9933be320dedc79025f6bb0bfd
Subham Mani <manisubham09@gmail.com>
2025-08-27 10:38:40 +0530

    refactor SocketProvider to use state and effect for socket initialization and cleanup

--- client/src/providers/Socket.tsx
+++ client/src/providers/Socket.tsx
@@ -1,19 +1,34 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { createContext, useContext, useMemo } from "react";
-import { io } from "socket.io-client";
+"use client";
 
-const SocketContext = createContext<any>(null);
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { io, Socket } from "socket.io-client";
+
+const SocketContext = createContext<Socket | null>(null);
 
 export const useSocket = () => {
   const socket = useContext(SocketContext);
+  if (!socket) {
+    throw new Error("Socket not initialized");
+  }
   return socket;
 };
 
 const SocketProvider = ({ children }: { children: React.ReactNode }) => {
-  const socket = useMemo(() => io("http://localhost:8000"), []);
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const _socket = io("http://localhost:8000");
+    setSocket(_socket);
+
+    return () => {
+      _socket.disconnect();
+    };
+  }, []);
 
   return (
-    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+    <SocketContext.Provider value={socket}>
+      {socket ? children : null}
+    </SocketContext.Provider>
   );
 };
 
