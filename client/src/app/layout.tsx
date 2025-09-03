6c7833dff3198068b100341b187009320bdc3b69
Subham Mani <manisubham09@gmail.com>
2025-09-03 11:12:46 +0530

    add suppressHydrationWarning to RootLayout component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${robotoMono.className} antialiased`}>
         {children}
         <Toaster />
