fd49fe529e740d8839de85ae05dc97fd1ab6b3b1
Subham Mani <manisubham09@gmail.com>
2025-09-24 19:45:23 +0530

    update stream server url

--- client/src/providers/StreamSocket.tsx
+++ client/src/providers/StreamSocket.tsx
@@ -17,9 +17,7 @@ const StreamSocketProvider = ({ children }: { children: React.ReactNode }) => {
   const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
-    const _socket = io(
-      "https://ec2-43-205-94-200.ap-south-1.compute.amazonaws.com:8001"
-    );
+    const _socket = io("https://airwave.stream.subhammani.xyz");
     setSocket(_socket);
 
     return () => {
